a1bd9ac50f1377f788bea29663b4b1c4102dd382
shanedbutler <shanedbutler@gmail.com>
2022-10-27 16:06:17 -0400

    Add validation with model class annotations

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,12 +1,20 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(35)]
         public string Name { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
         public string Address { get; set; }
+        [Phone]
         public string Phone { get; set; }
+        [Required]
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
     }
